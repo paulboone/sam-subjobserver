d8ec53e462c303d5222708c0dae12001541a7050
Paul Boone <narcissus.pong@gmail.com>
2016-06-10 14:09:35 -0400

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="sjs",
-    version="0.1",
+    version="0.2",
     scripts=['bin/sjs_launch_workers.sh', 'bin/sjs_status.py'],
     install_requires=['redis'],
     packages=['sjs']
