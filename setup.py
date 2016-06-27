8b5cc6e28ae4b2ea4ee8231f283802a8a7a4fb1d
Paul Boone <narcissus.pong@gmail.com>
2016-06-27 13:19:08 -0400

    give package a more specific name to avoid confusion

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
 setup(
-    name="sjs",
-    version="0.2",
+    name="wilmerlab-sjs",
+    version="0.2.1",
     scripts=['bin/sjs_launch_workers.sh', 'bin/sjs_status.py'],
     install_requires=['redis'],
     packages=['sjs']
