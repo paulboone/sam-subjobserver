0e94f00eb46893b24c0779c3ae6df0bfda0b3bd6
Paul Boone <narcissus.pong@gmail.com>
2016-08-24 15:02:15 -0400

    do env_record in python, and export as yaml

---
+++ sjs/scripts/env_record.py
@@ -0,0 +1,29 @@
+import subprocess
+import sys
+
+import click
+import yaml
+
+def _run_cmd(cmd_list):
+    results = subprocess.run(cmd_list, stdout=subprocess.PIPE, check=True, universal_newlines = True,)
+    return results.stdout.strip()
+
+def create_env_record():
+    env = {}
+
+    env['python_path'] = _run_cmd(['which', 'python'])
+    env['python_version'] = _run_cmd(['python', '--version'])
+
+    env['pip_path'] = _run_cmd(['which', 'pip'])
+    env['pip_version'] = _run_cmd(['pip', '--version'])
+    env['pip_packages'] = _run_cmd(['pip', 'list']).split('\n')
+
+    env['git_describe'] = _run_cmd(['git', 'describe', '--tags'])
+    env['git_commit'] = _run_cmd(['git', 'rev-parse', 'HEAD'])
+
+    return env
+
+@click.command()
+def env_record():
+    env = create_env_record()
+    sys.stdout.write(yaml.dump(env, default_flow_style=False))
