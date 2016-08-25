0e92d13babd140bc8b70e5c8632d87e26621cd96
Paul Boone <narcissus.pong@gmail.com>
2016-08-25 10:35:41 -0400

    store env_record when launch workers is called and hard-fail if it doesn't match the env_record at beginning of the run

--- sjs/scripts/launch_workers.py
+++ sjs/scripts/launch_workers.py
@@ -7,6 +7,8 @@ import sys
 import click
 
 import sjs
+from sjs.run import get_sjs_running_file, SJS_RUNNING_FILE
+from sjs.env_record import save_env_record, read_env_record
 
 def signal_handler(signal_received, frame):
     signal.signal(signal.SIGINT, signal.SIG_IGN)
@@ -31,13 +33,31 @@ def launch_workers(num_workers, burst, run_pre_check):
     else:
         print("Skipping pre-checks!")
 
+    working_dir = get_sjs_running_file()
+    if not working_dir:
+        raise SystemExit("Currently there is no run started (i.e. there is no %s file). " \
+            "Are you in the correct directory?" % SJS_RUNNING_FILE)
+
     hostname = os.uname()[1]
     timestamp = datetime.now().strftime("%Y_%m_%d__%H_%M_%S")
+
+    # compare env_record at start of run with this one
+    env_record_dir = os.path.join(working_dir, 'env_records')
+    env_record_path = os.path.join(env_record_dir, "%s_%s" %(hostname, timestamp))
+    env = save_env_record(env_record_path)
+    orig_env_record = read_env_record(os.path.join(env_record_dir, 'env_record_start.yaml'))
+    if env != orig_env_record:
+        print("env_record of this machine does not match env record of original machine! " \
+            "Aborting launch workers! Please see %s to compare manually" % (env_record_path))
+        raise SystemExit("Env records do not match, aborting launch workers!")
+
+
 
     print("")
     print("Running on hostname %s" % hostname)
     print("Running at timestamp %s" % timestamp)
     print("Log name template: %s_%s_*.log" % (hostname, timestamp))
+    print("Env record path: %s" % env_record_path)
 
     print("")
     rq_args = ""
